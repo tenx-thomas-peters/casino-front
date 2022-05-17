99952c1d688724e6f751cb7df170c56b1e817425
Thomas Peters <pianist.chopin.1@gmail.com>
2022-05-17 16:14:05 -0400

    service and support

--- src/@jumbo/components/AppLayout/partials/Header/index.js
+++ src/@jumbo/components/AppLayout/partials/Header/index.js
@@ -4,7 +4,30 @@ import {Link as RouterLink} from 'react-router-dom';
 import {NotificationManager} from 'react-notifications';
 
 import Toolbar from '@material-ui/core/Toolbar';
-import {makeStyles, Box, Grid, Tooltip, Typography, Link, Button, alpha, TextField} from '@material-ui/core';
+import {
+    makeStyles,
+    Box,
+    Grid,
+    Tooltip,
+    Typography,
+    Link,
+    Button,
+    alpha,
+    TextField,
+    Dialog,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    DialogActions,
+    useMediaQuery,
+    useTheme,
+    Menu,
+    MenuItem,
+    FormControlLabel,
+    Checkbox
+
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import SidebarToggleHandler from '../../../../../@coremat/CmtLayouts/Vertical/SidebarToggleHandler';
 import LanguageSwitcher from '../LanguageSwitcher';
 
@@ -13,6 +36,7 @@ import {AuthMethods} from "../../../../../services/auth";
 import {CurrentAuthMethod} from "../../../../constants/AppConstants";
 import clsx from 'clsx';
 import commonStyles from '../../../../common/common.style';
+import HomeAPI from '../../../../../services/api/apps/home';
 
 import axios from "axios";
 import gameService from '../../../../../services/gameConfig';
@@ -103,6 +127,61 @@ const useStyles = makeStyles(theme => ({
         '& fieldset': {
             borderColor: '#f44336'
         }
+    },
+
+    signUpModal: {
+        '& .MuiPaper-root': {
+            borderRadius: '10px',
+            backgroundColor: '#2f2f38',
+            minWidth: '400px',
+            paddingBottom: '20px'
+        },
+        '& #responsive-dialog-title span': {
+            cursor: 'pointer',
+            float: 'right',
+            padding: '10px 10px 5px',
+            background: '#181a27',
+            borderBottomLeftRadius: '10px',
+            color: '#efb221'
+        }
+    },
+    referralField: {
+        '& fieldset': {
+            borderRadius: '10px'
+        }
+    },
+    inlineNotice: {
+        whiteSpace: 'nowrap',
+        width: '300px',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        display: 'inline-block',
+        verticalAlign: 'middle',
+        '@media screen and (max-width: 1700px)': {
+            width: '200px'
+        },
+        '@media screen and (max-width: 1143px)': {
+            display: 'none'
+        }
+    },
+    popupDialog: {
+        '& .MuiBackdrop-root': {
+            backgroundColor: 'transparent'
+        },
+        '& .MuiDialog-paper': {
+            position: 'absolute',
+            minWidth: '450px',
+            left: '304px',
+            top: '60px',
+            backgroundColor: '#151a1d'
+        },
+        '& .MuiDialogActions-root': {
+            backgroundColor: '#77787b'
+        }
+    },
+
+    mpointer:{
+        cursor: "pointer"
     }
 }));
 
@@ -117,12 +196,45 @@ const Header = ({method = CurrentAuthMethod, commonInfo}) => {
     const [moneyAmount, setMoneyAmount] = useState(0);
     const [casinoMoney, setCasinoMoney] = useState(0);
     const [mileageAmount, setMileageAmount] = useState(0);
+    const [inlineNotice, setInlineNotice] = useState(0);
+    const [popupNotice, setPopupNotice] = useState([]);
 
     const [id, setId] = useState('');
     const [idError, setIdError] = useState(false);
 
     const [password, setPassword] = useState('');
     const [passwordError, setPasswordError] = useState(false);
+
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const [referralCode, setReferralCode] = useState('');
+    const [referralCodeError, setReferralCodeError] = useState('');
+
+    const [isRecommeded, setIsRecommeded] = useState(false);
+    const [isPopupShowed, setIsPopupShowed] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setReferralCode('');
+        setTimeout(() => {
+            setIsRecommeded(false);
+        }, 500);
+    };
+
+    const handleNext = () => {
+        if (referralCode.trim() === '') {
+            setReferralCodeError(true);
+        } else {
+            setReferralCodeError(false);
+            setIsRecommeded(true);
+        }
+    };
 
     let totalMoney = 0;
     useEffect(() => {
@@ -138,6 +250,15 @@ const Header = ({method = CurrentAuthMethod, commonInfo}) => {
             setMoneyAmount(commonInfo.moneyAmount);
             setCasinoMoney(commonInfo.casinoMoney);
             setMileageAmount(commonInfo.mileageAmount);
+
+            if (!isPopupShowed) {
+                setIsPopupShowed(localStorage.getItem("hidePopup") === "true" ? true : false);
+            }
+
+            if (!isPopupShowed && popupNotice !== undefined && popupNotice.length !== 0) {
+                setIsPopupShowed(true);
+                setPopupOpen(true);
+            }
         }
     }, [commonInfo]);
 
@@ -198,6 +319,82 @@ const Header = ({method = CurrentAuthMethod, commonInfo}) => {
             // .catch(function (err) {
             //     NotificationManager.error(err.message, 'Error');
             // });
+    };
+
+    const exchangeMileage= (event) => {
+        let userSeq = member.seq;
+        console.log(userSeq);
+        HomeAPI.exchangePoint({userSeq})
+            .then((res) => {
+                if (res.data.success) {
+                    setMember(res.data.result)
+                    // console.log(res.data.result);
+                }
+            })
+    }
+
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    const onSubmit = () => {
+        if (name.trim() === '' || nickname.trim() === '' || password.trim() === '') {
+            NotificationManager.error('Please input required field!', 'Error');
+            return;
+        } else {
+            dispatch(AuthMethods[method].onRegister(name, name, nickname, password));
+
+            setTimeout(() => {
+                window.location.reload();
+            }, 1000);
+        }
+    };
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const infoClose = () => {
+        setAnchorEl(null);
+    };
+
+    const descriptionElementRef = React.useRef(null);
+    const [popupOpen, setPopupOpen] = useState(false);
+
+    React.useEffect(() => {
+        if (popupOpen) {
+            const { current: descriptionElement } = descriptionElementRef;
+            if (descriptionElement !== null) {
+                descriptionElement.focus();
+            }
+        }
+    }, [popupOpen]);
+
+    const handlePopupClose = () => {
+        setPopupOpen(false);
+    };
+
+    const [state, setState] = React.useState({
+        checkedB: false
+    });
+
+    const handleChange = event => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+        localStorage.setItem("hidePopup", event.target.checked);
+    };
+
+    const renderRow = (item, index) => {
+        return (
+            <span key={index} style={{display: 'block'}}>
+                <span style={{margin: '20px 0', display: 'block'}}>
+                    {item.title}
+                </span>
+                <span>
+                    {item.content}
+                </span>
+            </span>
+        );
     };
 
     return (
@@ -275,13 +472,15 @@ const Header = ({method = CurrentAuthMethod, commonInfo}) => {
                             <Grid container>
                                 <Grid item md={12}>
                                     <i className={clsx(classes.user, 'iconfont icon-hearts')}></i>
-                                    <span>
-                                <IntlMessages id={"header.point"}/>
-                                <span style={{color: '#bb2322'}}>{mileageAmount ? mileageAmount : 0}</span>&nbsp;
-                                        <IntlMessages id={"header.point"}/>
-                                <i className={clsx('iconfont icon-zhuanhuan4')}
-                                   style={{color: '#757978', marginLeft: '5px'}}></i>
-                            </span>
+                                    <a onClick={exchangeMileage} className={classes.mpointer}>
+                                        <span>
+                                            <IntlMessages id={"header.point"}/>
+                                            <span style={{color: '#bb2322'}}>{mileageAmount ? mileageAmount : 0}</span>&nbsp;
+                                                    point
+                                            <i className={clsx('iconfont icon-zhuanhuan4')}
+                                               style={{color: '#757978', marginLeft: '5px'}}></i>
+                                        </span>
+                                    </a>
                                 </Grid>
                             </Grid>
                         </Tooltip>
@@ -322,9 +521,116 @@ const Header = ({method = CurrentAuthMethod, commonInfo}) => {
                     ''
                     :
                     <Box className={classes.langRoot}>
-                        <a href="/signup" className={clsx(classes.logoutBtn, commonClasses.hiddenXs)}><IntlMessages id={'header.signup'}/></a>
+                        <span className={clsx(classes.logoutBtn, commonClasses.hiddenXs)} onClick={handleClickOpen}><IntlMessages id={'header.signup'}/></span>
+                        <Dialog className={classes.signUpModal} fullScreen={fullScreen} open={open} onClose={handleClose} aria-labelledby="responsive-dialog-title">
+                            <DialogTitle id="responsive-dialog-title" style={{padding: '0'}}>
+                                <span onClick={handleClose}><CloseIcon /></span>
+                            </DialogTitle>
+                            <DialogContent>
+                                {
+                                    isRecommeded
+                                        ?
+                                        <form>
+                                            <Box mb={2}>
+                                                <TextField
+                                                    label={<IntlMessages id="appModule.name"/>}
+                                                    fullWidth
+                                                    onChange={event => setName(event.target.value)}
+                                                    defaultValue={name}
+                                                    margin="normal"
+                                                    variant="outlined"
+                                                    className={classes.textFieldRoot}
+                                                    required={true}
+                                                />
+                                            </Box>
+                                            <Box mb={2}>
+                                                <TextField
+                                                    label={<IntlMessages id="appModule.nickname"/>}
+                                                    fullWidth
+                                                    onChange={event => setNickname(event.target.value)}
+                                                    defaultValue={nickname}
+                                                    margin="normal"
+                                                    variant="outlined"
+                                                    className={classes.textFieldRoot}
+                                                    required={true}
+                                                />
+                                            </Box>
+                                            <Box mb={2}>
+                                                <TextField
+                                                    type="password"
+                                                    label={<IntlMessages id="appModule.password"/>}
+                                                    fullWidth
+                                                    onChange={event => setPassword(event.target.value)}
+                                                    defaultValue={password}
+                                                    margin="normal"
+                                                    variant="outlined"
+                                                    className={classes.textFieldRoot}
+                                                    required={true}
+                                                />
+                                            </Box>
+                                            <Box
+                                                display="flex"
+                                                flexDirection={{xs: 'column', sm: 'row'}}
+                                                alignItems={{sm: 'center'}}
+                                                justifyContent={{sm: 'space-between'}}
+                                                mb={3}>
+                                                <Box mb={{xs: 2, sm: 0}}>
+                                                    <Button onClick={onSubmit} variant="contained" color="primary">
+                                                        <IntlMessages id="appModule.regsiter"/>
+                                                    </Button>
+                                                </Box>
+                                            </Box>
+                                        </form>
+                                        :
+                                        <Box pr={2} className={commonClasses.hiddenSm} style={{padding: '0 20px'}}>
+                                            <h2 style={{color: '#efb221', textAlign:'center', fontSize: '40px', fontWeight: '900', marginBottom: '20px'}}><IntlMessages id={'signup.title'}/></h2>
+                                            <span><IntlMessages id={'signup.referralCode'}/></span>
+                                            <TextField
+                                                type="text"
+                                                label={<IntlMessages id="signup.referralCodeInput"/>}
+                                                fullWidth
+                                                onChange={event => setReferralCode(event.target.value)}
+                                                defaultValue={referralCode}
+                                                margin="normal"
+                                                variant="outlined"
+                                                className={referralCodeError ? clsx(classes.textFieldError, classes.referralField) : classes.referralField}
+                                                required
+                                                style={{background: '#1e1f21', borderRadius: '10px'}}
+                                            />
+                                            <Button onClick={handleNext} className={commonClasses.containedActionButton} autoFocus style={{width: '100%', padding: '12px 0', marginTop: '15px', borderRadius: '10px'}}>
+                                                <IntlMessages id={'signup.next'}/>
+                                            </Button>
+                                        </Box>
+                                }
+                            </DialogContent>
+                        </Dialog>
                     </Box>
             }
+
+            <Dialog
+                className={classes.popupDialog}
+                open={popupOpen}
+                onClose={handleClose}
+                scroll={'paper'}
+                aria-labelledby="scroll-dialog-title"
+                aria-describedby="scroll-dialog-description">
+                <DialogContent>
+                    <DialogContentText id="scroll-dialog-description" ref={descriptionElementRef} tabIndex={-1}>
+                        {
+                            popupNotice && popupNotice.length > 0 ? popupNotice.map((item, index) => renderRow(item, index)) : ''
+                        }
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <FormControlLabel
+                        style={{position: 'absolute', left: '10px'}}
+                        control={<Checkbox checked={state.checkedB} onChange={handleChange} name="checkedB" color="primary" />}
+                        label={<IntlMessages id={"home.popupAlarm"}/>}/>
+                    <Button onClick={handlePopupClose}>
+                        <IntlMessages id={"home.close"}/>
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Toolbar>
     );
 };
